9d5964325b17b9b0e10fe9bd3c6673a513ed5494
Dmytro Chumak <dimonchumak@gmail.com>
2019-03-03 18:30:05 -0400

    Add random scene generation

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,32 @@ fn color(r: Ray, world: &dyn Hitable, depth: i32) -> Vec3 {
     }
 }
 
+fn random_scene(rng: &mut rand::prelude::ThreadRng) -> Vec<Box<Hitable>> {
+    let mut hitable: Vec<Box<Hitable>> = vec![];
+    hitable.push(Box::new(Sphere::new(Vec3::new(0.0, -1000.0, 0.0), 1000.0, Rc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5))))));
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = Vec3::new(a as f32 + 0.9 * rng.gen::<f32>(), 0.2, b as f32 + 0.9 * rng.gen::<f32>());
+            let rand = rng.gen::<f32>();
+            if (center - Vec3::new(4.9, 0.2, 0.0)).length() > 0.9 {
+                if rand < 0.8 {
+                    hitable.push(Box::new(Sphere::new(center, 0.2, Rc::new(Lambertian::new(Vec3::new(rng.gen::<f32>() * rng.gen::<f32>(), rng.gen::<f32>() * rng.gen::<f32>(), rng.gen::<f32>()* rng.gen::<f32>()))))));
+                }
+                else if rand < 0.95 {
+                    hitable.push(Box::new(Sphere::new(center, 0.2, Rc::new(Metal::new(Vec3::new(0.5 * (1.0 + rng.gen::<f32>()), 0.5 * (1.0 + rng.gen::<f32>()), 0.5 * (1.0 + rng.gen::<f32>())))))));
+                }
+                else {
+                    hitable.push(Box::new(Sphere::new(center, 0.2, Rc::new(Dielectric::new(1.5)))));
+                }
+            }
+        }
+    }
+    hitable.push(Box::new(Sphere::new(Vec3::new(0.0, 1.0, 0.0), 1.0, Rc::new(Metal::new(Vec3::new(0.3, 0.9, 0.4))))));
+    hitable.push(Box::new(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, Rc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1))))));
+    hitable.push(Box::new(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, Rc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5))))));
+    hitable
+}
+
 fn main() {
     let file_name = "outpu.ppm";
     let file_path = Path::new(file_name);
@@ -55,24 +81,25 @@ fn main() {
         Err(e)      => panic!("Could not create file: {} error: {:?}", file_name, e.kind()),
     };
 
-    let width = 800;
-    let height = 400;
+    let width = 1200;
+    let height = 600;
 
     write!(file, "P3\n{} {}\n255\n", width,height).expect("Could not write to file");
-    let hitable: Vec<Box<Hitable>> = vec![
-        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, Rc::new(Lambertian::new(Vec3::new(0.8, 0.3, 0.3))))),
-        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))))),
-        Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2))))),
-        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, Rc::new(Dielectric::new(1.5)))),
-        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.45, Rc::new(Dielectric::new(1.5)))),
-    ];
+    let mut rng = rand::thread_rng();
+    let hitable: Vec<Box<Hitable>> = random_scene(&mut rng);
+        /*vec![
+            Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, Rc::new(Lambertian::new(Vec3::new(0.8, 0.3, 0.3))))),
+            Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))))),
+            Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2))))),
+            Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, Rc::new(Dielectric::new(1.5)))),
+            Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.45, Rc::new(Dielectric::new(1.5)))),
+        ];*/
 
     let world = HitableList::new(hitable);
-    let look_from = Vec3::new(3.0, 3.0, 2.0);
-    let look_at = Vec3::new(0.0, 0.0, -1.0);
-    let camera = Camera::new(look_from, look_at, Vec3::new(0.0, 1.0, 0.0), 20.0, (width as f32) / (height as f32), 2.0, (look_from - look_at).length());
+    let look_from = Vec3::new(13.0, 2.0, 3.0);
+    let look_at = Vec3::new(0.0, 0.0, 0.0);
+    let camera = Camera::new(look_from, look_at, Vec3::new(0.0, 1.0, 0.0), 20.0, (width as f32) / (height as f32), 0.1, 10.0);
 
-    let mut rng = rand::thread_rng();
     let num_samples = 100;
     
     for j in (0 .. height).rev() {
